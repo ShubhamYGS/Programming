75a6f655ac5c645791aee90cd1ca6033d63c723d
ShubhamYGS <33577947+ShubhamYGS@users.noreply.github.com>
2018-12-05 22:40:56 +0530

    Pattern in the form 111

---
+++ Pattern/Pattern1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			printf("%d",i);
+		}
+		printf("\n");
+	}	
+}
+
+/*Output:
+11111
+22222
+33333
+44444
+55555
+*/
