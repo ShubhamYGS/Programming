4f558c134a591d677d7cbfef7df3e44319863eb7
ShubhamYGS <33577947+ShubhamYGS@users.noreply.github.com>
2018-12-06 18:10:31 +0530

    Diagonal Pattern

---
+++ Pattern6/Simple.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+	int i,j,k;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(i==j)
+			{
+				printf(" *");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+}
+
+/*Output:
+
+ *
+   *
+     *
+       *
+         *
+*/
