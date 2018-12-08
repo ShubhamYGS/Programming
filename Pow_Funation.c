65bf0a1b10eb447aaeef7f7f7c4d7958e9792bc3
ShubhamYGS <s.patil14569@gmail.com>
2018-12-08 23:15:36 +0530

    Use of pow function

---
+++ Pow_Funation.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a;
+	a=pow(2,3);       //pow() function is a standard library function. Used for showing the power.
+					 //In this example it will calculate the 2^3
+	printf("%d",a);
+}
