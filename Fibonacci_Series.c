936f2524afcd4389dd8bbaa952176675e8431a5f
Babar Ganesh Ravso <40852387+macrotek@users.noreply.github.com>
2020-10-01 10:42:05 +0530

    Update Fibonacci_Series.c

--- Fibonacci_Series.c
+++ Fibonacci_Series.c
@@ -1,24 +1,16 @@
-#include<stdio.h>
-int main()
-{
-	int num,a=0,b=1,i,c;
-	
-	printf("Enter the number: ");
-	scanf("%d",&num);
-	
-	printf("Fibonacci Series: 0 1");
-	for(i=1;i<=num-2;i++)
-	{
-		c=a+b;
-		printf(" %d",c);
-		a=b;
-		b=c;
-	}
-	
-	return 0;
-}
-
-/*Output:
-Enter the number: 9
-Fibonacci Series: 0 1 1 2 3 5 8 13 21
-*/
+//Fibonacci Series using Recursion 
+#include<stdio.h> 
+int fib(int n) 
+{ 
+   if (n <= 1) 
+      return n; 
+   return fib(n-1) + fib(n-2); 
+} 
+  
+int main () 
+{ 
+  int n = 9; 
+  printf("%d", fib(n)); 
+  getchar(); 
+  return 0; 
+} 
